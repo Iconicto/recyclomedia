61344b49935318beabcde3e20ba39594bde82afa
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2020-06-28 14:26:23 +0530

    Completed Sign Up/In pages and added api models

--- recyclomedia/lib/pages/signup.page.dart
+++ recyclomedia/lib/pages/signup.page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:recyclomedia/pages/signup.page.dart';
+import 'package:recyclomedia/pages/login.page.dart';
 
-class LoginPage extends StatelessWidget {
+class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,16 +13,17 @@ class LoginPage extends StatelessWidget {
                 Container(
                   height: double.infinity,
                   width: double.infinity,
+                  color: Colors.red,
                   child: Image.asset(
-                    "lib/assets/images/login.jpg",
+                    "lib/assets/images/signup.png",
                     fit: BoxFit.fill,
                   ),
                 ),
                 Positioned(
-                  top: MediaQuery.of(context).viewPadding.top < 0 ? 65 : 40,
+                  top: 65,
                   left: 25,
                   child: Text(
-                    "Sign In",
+                    "Sign Up",
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 40,
@@ -82,6 +83,41 @@ class LoginPage extends StatelessWidget {
                         height: 20,
                         color: Colors.transparent,
                       ),
+                      Text("Email"),
+                      Divider(
+                        height: 5,
+                        color: Colors.transparent,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter your email here',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 5,
+                            ),
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 5,
+                            ),
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 5,
+                            ),
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        height: 20,
+                        color: Colors.transparent,
+                      ),
                       Text("Password"),
                       Divider(
                         height: 5,
@@ -114,23 +150,13 @@ class LoginPage extends StatelessWidget {
                         ),
                       ),
                       Divider(
-                        height: 10,
-                        color: Colors.transparent,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text("Forgot Password?"),
-                        ],
-                      ),
-                      Divider(
                         height: 30,
                         color: Colors.transparent,
                       ),
                       Container(
                         padding: EdgeInsets.symmetric(vertical: 20),
                         alignment: Alignment.center,
-                        child: Text("SIGN IN",
+                        child: Text("SIGN UP",
                             style: TextStyle(color: Colors.white)),
                         color: Colors.black,
                       ),
@@ -139,14 +165,14 @@ class LoginPage extends StatelessWidget {
                           Navigator.push(
                             context,
                             new MaterialPageRoute(
-                              builder: (context) => SignUpPage(),
+                              builder: (context) => LoginPage(),
                             ),
                           );
                         },
                         child: Container(
                           padding: EdgeInsets.symmetric(vertical: 20),
                           alignment: Alignment.center,
-                          child: Text("SIGN UP"),
+                          child: Text("OR SIGN IN?"),
                         ),
                       )
                     ],
