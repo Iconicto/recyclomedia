6bc97d92de48602e50b8500fa4bc554ef86fce66
Visal Rajapakse <visalrajapakse@gmail.com>
2020-06-28 12:37:56 +0530

    added navbar

--- recyclomedia/lib/pages/home.page.dart
+++ recyclomedia/lib/pages/home.page.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:recyclomedia/assets/fonts.dart';
+import 'package:recyclomedia/colors.dart';
+import 'package:recyclomedia/fonts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<HomePage> {
+
+  int _currentNavItem = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        child: Text(
-          "Hellos there",
-          style: TextStyle(fontFamily: Fonts.SilkScreen, fontSize: 100),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        onTap: onTabTapped,
+        currentIndex: _currentNavItem, // this will be set when a new tab is tapped
+        items: [
+          NavigationItem(Icons.home, "H", "ome"),
+          NavigationItem(Icons.flag, "E", "vents"),
+          NavigationItem(Icons.person, "P", "rofile"),
+          NavigationItem(Icons.settings, "S", "ettings"),
+        ],
       ),
-    ));
+    );
+  }
+
+  BottomNavigationBarItem NavigationItem(IconData icon, String underlinedNavTitleText, String navTitleText) {
+    return BottomNavigationBarItem(
+          icon: Icon(icon),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                underlinedNavTitleText,
+                style: TextStyle(
+                    fontFamily: Fonts.DisposableDroid,
+                    fontSize: FontSize.NavItem,
+                    decoration: TextDecoration.underline),
+              ),
+              Text(
+                navTitleText,
+                style: TextStyle(
+                    fontFamily: Fonts.DisposableDroid,
+                    fontSize: FontSize.NavItem),
+              )
+            ],
+          ),
+        );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentNavItem = index;
+    });
   }
 }
