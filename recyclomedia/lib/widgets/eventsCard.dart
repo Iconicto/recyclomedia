6b1abd2e2f524ddcd85c3ebb89a26498094c6345
Suvink <suvink456@yahoo.com>
2020-06-28 19:49:05 +0530

    Added Events page

--- recyclomedia/lib/widgets/eventsCard.dart
+++ recyclomedia/lib/widgets/eventsCard.dart
@@ -1,41 +1,31 @@
 import 'package:flutter/material.dart';
 
-
 //Boarding Cards
-class eventsCard extends StatelessWidget {
+class EventsCard extends StatelessWidget {
   final String eventName;
   final String imgurl;
 
-  const eventsCard(this.eventName, this.imgurl);
+  const EventsCard(this.eventName, this.imgurl);
 
   @override
   Widget build(BuildContext context) {
-
     //Viewport
     final vh = MediaQuery.of(context).size.height;
     final vw = MediaQuery.of(context).size.width;
 
     return new Container(
-      height: 84.0,
-      width: vw,
-      margin: EdgeInsets.only(top: 10.0),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(imgurl),
-          fit: BoxFit.cover,
-        ),
-        border: Border.all(color: Colors.black, width: 5)
-      ),
-      child: Center(
-        child: Text(
-          eventName,
-          style: TextStyle(
-            fontFamily: 'SilkScreen',
-            color: Colors.white
-            )
-          )
-      )
-    );
+        height: 84.0,
+        width: vw,
+        margin: EdgeInsets.only(top: 10.0),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(imgurl),
+              fit: BoxFit.cover,
+            ),
+            border: Border.all(color: Colors.black, width: 5)),
+        child: Center(
+            child: Text(eventName,
+                style:
+                    TextStyle(fontFamily: 'SilkScreen', color: Colors.white))));
   }
 }
-
